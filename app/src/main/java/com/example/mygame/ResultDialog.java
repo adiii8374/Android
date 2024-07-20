7405070671b1caa1df05cc857d0840774db51a8e
adiii8374 <155254708+adiii8374@users.noreply.github.com>
2024-07-20 23:16:32 +0530

    Final Commit

---
+++ app/src/main/java/com/example/mygame/ResultDialog.java
@@ -0,0 +1,42 @@
+package com.example.mygame;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultDialog extends Dialog {
+
+    private final String message;
+    private final MainActivity2 mainActivity2;
+
+    public ResultDialog(@NonNull Context context, String message, MainActivity2 mainActivity2) {
+        super(context);
+        this.message = message;
+        this.mainActivity2 = mainActivity2;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_dialog);
+
+        TextView messageText = findViewById(R.id.messageText);
+        Button startAgainButton = findViewById(R.id.startAgainButton);
+
+        messageText.setText(message);
+
+        startAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity2.restartMatch();
+                dismiss();
+            }
+        });
+    }
+}
